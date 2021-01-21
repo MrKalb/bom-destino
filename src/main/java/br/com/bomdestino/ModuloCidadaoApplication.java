87aab6f8e13932323364e9bbfd30e69e60ca1dbe
Igor Kalb <ikalb9@gmail.com>
2021-01-20 22:03:59 -0300

    First commit
    first commit

---
+++ src/main/java/br/com/bomdestino/ModuloCidadaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bomdestino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloCidadaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloCidadaoApplication.class, args);
+	}
+
+}
